4f03eec3176e6f2f51ff83190e2ddf583a9bef9c
Jasjeev <jasjeev99@gmail.com>
2023-11-25 01:10:51 +0530

    added modules

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -36,6 +36,7 @@ const products = [{
 
 import {cart} from "../data/cart.js";
 import {products} from "../data/products.js";
+import { formatCurrency } from "./utils/money.js";
 
 addProducts();
 function addProducts() {
@@ -62,7 +63,7 @@ function addProducts() {
         </div>
     
         <div class="product-price">
-        $${(products[i].priceCents / 100).toFixed(2)}
+        $${formatCurrency(products[i].priceCents)}
         </div>
     
         <div class="product-quantity-container">
